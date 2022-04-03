fc123fbca1f94facd453e95078c128c608c8fc84
MelaHub <pamela.gotti@gmail.com>
2022-04-03 18:04:59 +0200

    first-: Added command to find the first contribution

--- calcifer/calcifer.py
+++ calcifer/calcifer.py
@@ -7,51 +7,13 @@ from datetime import datetime
 import itertools
 from tqdm import tqdm
 
-REPO_PAGE_SIZE = 100
+from calcifer.services.github import get_all_repos, get_contributors_for_repo, get_commits_for_repo
+
 
 def print_iterator(it):
     for x in it:
         print(x, end=' ')
     print('') 
-
-def get_all_pages(url, query_params, github_user, github_token, stop_if=lambda x: False, page_size=REPO_PAGE_SIZE, max_results=None):
-    page_size = page_size if max_results is None or max_results > page_size else max_results
-    results = []
-    query_params.update({
-        'per_page': page_size,
-        'page': 0
-    })
-    while query_params['page'] is not None and (max_results is None or max_results is not None and len(results) < max_results):
-        query_params_str = '&'.join([f'{k}={query_params[k]}' for k in query_params])
-        response = requests.get(f'{url}?{query_params_str}', auth = HTTPBasicAuth(github_user, github_token))
-        if response.status_code not in (200, 404, 409):
-            return []
-            raise Exception("Something went wrong while calling the github api")
-        if response.status_code in (404, 409):
-            return []
-        curr_res = json.loads(response.content)
-        if len(curr_res):
-            query_params['page'] += 1
-            valid_results = [res for res in curr_res if not stop_if(res)]
-            results += valid_results
-            if len(valid_results) < len(curr_res):
-                break
-        else:
-            query_params['page'] = None
-    return results
-
-def get_all_repos(github_org, github_user, github_token):
-    print(f'Retrieving all {github_org} repos - hold on...')
-    return get_all_pages(f'https://api.github.com/orgs/{github_org}/repos', {}, github_user, github_token)
-
-def get_contributors_for_repo(repo, github_user, github_token):
-    response = requests.get(repo['contributors_url'].replace('{/collaborator}', ''), auth = HTTPBasicAuth(github_user, github_token))
-    content = []
-    if response.status_code != 200 and response.status_code != 204:
-        raise Exception(f"Something went wrong while fetching details of repo {repo}")
-    elif response.status_code == 200:
-        content = json.loads(response.content)
-    return content
 
 def get_top_contributors_for_repo(repo, github_user, github_token, n_contrib):
     content = get_contributors_for_repo(repo, github_user, github_token)
@@ -60,12 +22,6 @@ def get_top_contributors_for_repo(repo, github_user, github_token, n_contrib):
         'total_commits': sum([x['contributions'] for x in content]),
         'contributors': [{'login': c['login'], 'contributions': c['contributions']} for c in content[0:n_contrib]]
     }
-
-def get_commits_for_repo(repo, github_user, github_token):
-    stop_if = lambda x: datetime.strptime(x['commit']['author']['date'], '%Y-%m-%dT%H:%M:%SZ') < datetime(2016, 1, 1)
-    main_response = get_all_pages(repo['commits_url'].replace('{/sha}', ''), {'sha': 'main'}, github_user, github_token, stop_if=stop_if)
-    master_response = get_all_pages(repo['commits_url'].replace('{/sha}', ''), {'sha': 'master'}, github_user, github_token, stop_if = stop_if)
-    return main_response + master_response
 
 def write_main_contributors_to_file(contributors_repo, out_file_path):
     def get_key_by_index_or_empty(contributors, index, key):
