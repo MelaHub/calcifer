d7cccdae1c75f2193799e0b440fda56dfc2cb712
MelaHub <pamela.gotti@gmail.com>
2022-04-03 21:21:15 +0200

    first-: Get commands to get releases

--- calcifer/calcifer.py
+++ calcifer/calcifer.py
@@ -1,4 +1,5 @@
 import json
+from turtle import pd
 import requests
 from requests.auth import HTTPBasicAuth
 import click
@@ -7,7 +8,7 @@ from datetime import datetime
 import itertools
 from tqdm import tqdm
 
-from calcifer.services.github import get_all_repos, get_contributors_for_repo, get_commits_for_repo
+from calcifer.services.github import get_all_repos, get_contributors_for_repo, get_commits_for_repo, get_commits_for_repo_with_tag, get_commit_with_sha
 
 
 def print_iterator(it):
@@ -75,6 +76,32 @@ def get_first_contribution_by_repo(repo, github_user, github_token):
         }
         for author, commits in itertools.groupby(date_commits, lambda x: x['author'])}
 
+def get_commits_with_tag(repo, github_user, github_token, tag):
+    commits_with_tag = get_commits_for_repo_with_tag(repo, github_user, github_token, tag)
+    commits_with_tag_details = []
+    for commit in commits_with_tag:
+        commit_details = get_commit_with_sha(repo, github_user, github_token, commit['commit']['sha'])
+        commits_with_tag_details.append(
+            {
+                'repo': repo['name'],
+                'tag': commit['name'],
+                'author': commit_details['commit']['author']['name'],
+                'message': commit_details['commit']['message'].replace('\n', '; '),
+                'date': commit_details['commit']['author']['date']
+            }
+        )
+    return commits_with_tag_details
+
+def write_commits_on_file(commit_details, out_file_path):
+    with open(out_file_path, 'w') as csvfile: 
+        writer = csv.DictWriter(csvfile, fieldnames=commit_details[0].keys())
+
+        writer.writeheader()
+        for commit in commit_details:
+            writer.writerow(commit)
+
+    print(f'I\'ve written {len(commit_details)} to {out_file_path}')
+
 def write_first_contribution_to_file(first_contributions, out_file_path):
     def get_key_by_index_or_empty(contributors, index, key):
         return contributors['contributors'][index][key] if len(contributors['contributors']) >= (index + 1) else ''
@@ -115,9 +142,22 @@ def first_contribution(github_user, github_token, github_org, out_file_path):
                 first_contributions[author] = contribution[author]
     write_first_contribution_to_file(first_contributions, out_file_path)
 
+@click.command()
+@click.option("--github-user", envvar="GITHUB_USER", type=str, required=True)
+@click.option("--github-token", envvar="GITHUB_TOKEN", type=str, required=True)
+@click.option("--github-org", type=str, required=True)
+@click.option("--out-file-path", type=str, required=True)
+def audit_releases(github_user, github_token, github_org, out_file_path):
+    repos = get_all_repos(github_org, github_user, github_token)
+    print(f'Found {len(repos)} repositories')
+    print(f'Retrieving now all releases of this year...')
+    commits = [c for c in map(lambda x: get_commits_with_tag(x, github_user, github_token, 'release-2021'), tqdm([repos]))]
+    write_commits_on_file([c for commits_per_repo in commits for c in commits_per_repo], out_file_path)
+
 @click.group()
 def cli():
     pass
 
 cli.add_command(main_contributors)
 cli.add_command(first_contribution)
+cli.add_command(audit_releases)
